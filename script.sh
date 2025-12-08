6c1677be13c4b483e77033b951b47a4e4c8fdcfb
lucachak <lucas.lucachak9@gmail.com>
2025-12-08 12:14:42 +0000

    fixing the script

--- script.sh
+++ script.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-# Get the IP address from your script
-
 
 IP=$(./helpers/get_ip.sh)
 echo
 
 
-# Run Django server with the IP and port
-python manage.py runserver $IP:8000
+source .venv/bin/activate; python3 manage.py runserver $IP:8000
